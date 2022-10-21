d9f1aa7683ec1ac7827cf300a24180b6f1beae2f
JO <110362625+Joichi508@users.noreply.github.com>
2022-10-21 15:33:24 -0700

    Add files via upload

---
+++ website/index.php
@@ -0,0 +1,25 @@
+<?php include('./includes/header.php');?>
+
+<div id="wrapper">
+<div id="hero">
+<img src="images/twelve.jpg" alt="Twelve is greater than 3">
+</div>
+<main>
+    <h1>Welcome to our Web App Programming Class!</h1>
+        <h2>We are going to learn PHP!</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porttitor pellentesque aliquam. Nam elementum congue risus sit amet semper. Vivamus efficitur magna sed faucibus aliquet. Nullam hendrerit, augue non ullamcorper tempor, lorem magna pellentesque dui, interdum ornare enim lorem eget sapien. Etiam non feugiat urna. Sed non magna et tortor cursus semper non sed nisi. Etiam in placerat augue. Nullam ultricies imperdiet nisl, sit amet tincidunt lorem maximus sit amet. Sed at turpis rhoncus est facilisis aliquam. Etiam nulla justo, tincidunt a mi at, lobortis ornare ante. Nulla quis nunc augue. Etiam viverra, libero ut suscipit congue, orci felis elementum lorem, ac mollis mi magna vel risus. Sed vehicula urna aliquet tellus fermentum aliquet. Nulla egestas, nisi vitae lacinia euismod, turpis felis rutrum est, mattis faucibus ligula nunc et nisl. Vivamus efficitur lorem nec accumsan gravida.</p>
+        <h2>Another Headline</h2>
+            <p>Nam dapibus, mi vitae volutpat euismod, tortor sem tempor turpis, a tristique ex purus ut elit. Cras lacinia tincidunt sapien. Morbi faucibus lacinia magna at volutpat. Sed condimentum sit amet urna sit amet vulputate. Sed eget scelerisque tortor, sit amet tempor augue. Mauris ut felis eu ligula viverra viverra a sit amet est. Nulla facilisis imperdiet consectetur. Aenean pretium, nisl in efficitur rutrum, risus tellus hendrerit diam, at luctus nisl erat non tortor. Aenean urna lectus, tincidunt nec lacus eget, mattis volutpat dui.</p>
+</main>
+<aside>
+<h3>This is our headline three in our beautiful aside</h3>
+<p>Nullam ultricies imperdiet nisl, sit amet tincidunt lorem maximus sit amet. Sed at turpis rhoncus est facilisis aliquam. Etiam nulla justo, tincidunt a mi at, lobortis ornare ante. Nulla quis nunc augue. Etiam viverra, libero ut suscipit congue, orci felis elementum lorem, ac mollis mi magna vel risus. Sed vehicula urna aliquet tellus fermentum aliquet. Nulla egestas, nisi vitae lacinia euismod, turpis felis rutrum est, mattis faucibus ligula nunc et nisl. Vivamus efficitur lorem nec accumsan gravida.</p>
+
+
+
+</aside>
+
+</div>
+
+<?php
+include('./includes/footer.php'); 
