b37fe504cbd3c05069ec78b22fcb396ba73310c1
JO <110362625+Joichi508@users.noreply.github.com>
2022-11-28 03:35:18 -0800

    Add files via upload

---
+++ website/gallery.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gallery</title>
+    <link href="css/styles.css" type="text/css" rel="stylesheet">
+    <style>
+        table{
+            border:1px solid black;
+            border-collapse:collapse;
+        }
+
+        td {
+            border: 1px solid black;
+            padding:5px;
+        }
+
+        img{
+            width: 200px;
+            height: 200px;
+        }
+    </style>
+</head>
+
+<?php 
+include('./includes/header.php'); ?>
+
+<?php
+$wcup ['GAVI'] = 'Barcelona Spain age 18';
+$wcup['MBAPPE'] = 'Paris Saint-Germain France age 23';
+$wcup['MITOMA'] = 'Brighton Japan age 25';
+$wcup['MUSIALA'] = ' Bayern Munich Germany age 19';
+$wcup ['PULISIC'] = ' Chelsea United States age 24';
+?>
+
+
+<div id="wrapper">
+<table>
+        <?php foreach($wcup as $name => $image) :?>
+        <tr>
+            <td><img src="img/<?php echo $name;?>.jpg" alt="<?php echo $name;?>"></td>
+            <td><?php echo str_replace('_',' ', $name);?></td>
+            <td><?php echo substr($image, 0, 45);?></td>
+        </tr>
+        <?php endforeach ;?>
+    </table>
+<?php
+include('./includes/footer.php')
+?>
