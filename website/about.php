f5acdf08226bb05d99445cd7ca64250c06fea5c5
JO <110362625+Joichi508@users.noreply.github.com>
2022-12-10 02:02:48 -0800

    Add files via upload

---
+++ website/about.php
@@ -0,0 +1,8 @@
+<?php
+include('config.php');
+include('./includes/header.php');
+?>
+<h2>Welcome to My page!</h2>
+<a href=""><img src="images/db.jpg" alt="db"> </a>
+<?php
+include('./includes/footer.php');
---
+++ website/credentials.php
@@ -0,0 +1,4 @@
+project name = jboydayo_it261
+user name = jboydayo
+password = joichi-0508
+host = mysql641.db.sakura.ne.jp
