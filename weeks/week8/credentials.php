824056672135aafa2e02345d39391045f64f7693
JO <110362625+Joichi508@users.noreply.github.com>
2022-12-11 00:30:40 -0800

    Add files via upload

---
+++ weeks/week8/credentials.php
@@ -0,0 +1,8 @@
+
+<?php
+define('DB_NAME', 'jboydayo_it261');
+define('DB_USER', 'jboydayo');
+define('DB_PASSWORD', 'joichi-0508');
+define('DB_HOST', 'mysql641.db.sakura.ne.jp');
+
+?>
---
+++ weeks/week9/login.php
@@ -0,0 +1,26 @@
+<?php
+include('server.php');
+include('includes/header.php'); ?>
+
+<div id="wrapper">
+
+<h1 class="center">Login</h1>
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ; ?>" method="post" >
+    <fieldset>
+        <label>Username</label>
+        <input type="text" name="username" value="<?php if(isset($_POST['username'])) echo $_POST['username'] ;?>">
+        <label>Password</label>
+        <input type="password" name="password">
+
+        <button type="submit" class="btn" name="login_user">Login</button>
+
+        <button type="button" onclick="window.location.href='<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ;?>' ">Reset</button>
+
+        <?php include('errors.php');?>
+    </fieldset>
+</form>
+    <h3>Not a member?</h3>
+    <span class="block"><a href="register.php">Register Page!</a></span>
+
+</div>
+<?php include('includes/footer.php');
