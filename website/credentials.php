44dc7792c9b93991fa190dde62900043b770e006
JO <110362625+Joichi508@users.noreply.github.com>
2022-12-12 05:33:24 -0800

    Add files via upload

--- website/credentials.php
+++ website/credentials.php
@@ -1,4 +1,8 @@
-project name = jboydayo_it261
-user name = jboydayo
-password = joichi-0508
-host = mysql641.db.sakura.ne.jp
+
+<?php
+define('DB_NAME', 'jboydayo_it261');
+define('DB_USER', 'jboydayo');
+define('DB_PASSWORD', 'joichi-0508');
+define('DB_HOST', 'mysql641.db.sakura.ne.jp');
+
+?>
